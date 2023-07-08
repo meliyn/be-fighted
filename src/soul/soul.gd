e2899ccf8c3fbfb32441e46e0e36fa0a218fedbd
adenpun <adenpun2000@gmail.com>
2023-07-08 10:46:52 +0800

    Tryin to move

--- src/soul/soul.gd
+++ src/soul/soul.gd
@@ -2,6 +2,8 @@ class_name Soul
 extends CharacterBody2D
 
 const SPEED = 300.0
+var vector: Vector2
+@onready var tree = get_tree()
 
 
 func _enter_tree():
@@ -13,6 +15,21 @@ func _ready():
 
 
 func _physics_process(_delta):
+	var closest: Node2D = _get_closest_attack()
+	if closest != null:
+		var original_rotation = rotation
+		look_at(closest.position)
 	# velocity.x = SPEED
 
 	move_and_slide()
+
+
+func _get_closest_attack() -> Node2D:
+	var closest: Node2D
+	var closest_distance: float = -1
+	for attack in tree.get_nodes_in_group("attack"):
+		var distance = position.distance_to(attack.position)
+		if closest_distance == -1 or distance < closest_distance:
+			closest = attack
+			closest_distance = distance
+	return closest
